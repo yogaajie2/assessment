6e67dab123e9ad19e35dc4e350d94fc8e235955c
yogaajie2 <yogaajie2@gmail.com>
2022-01-24 23:16:22 +0700

    : static view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable quote-props */
 module.exports = {
   purge: [
     './components/**/*.{vue,js}',
@@ -8,7 +9,27 @@ module.exports = {
   ],
   content: [],
   theme: {
-    extend: {}
+    extend: {
+      spacing: {
+        '23': '5.75rem'
+      }
+    },
+
+    colors: {
+      primary: '#1C1C1C',
+      secondary: {
+        DEFAULT: '#676767',
+        light: '#ACACAC',
+        lighter: '#C5C5C5',
+        lightest: '#F0EFEF'
+      },
+      white: '#FFF',
+      red: '#EE6352',
+      blue: '#00A5CF',
+      green: '#00B14F',
+      gray: '#F9FBFD',
+      darkgray: '#F7F7F7'
+    }
   },
   plugins: []
 }
