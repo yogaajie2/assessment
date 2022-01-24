25031743bdcb20a6b98893867af36abb67792d88
yogaajie2 <yogaajie2@gmail.com>
2022-01-25 00:22:19 +0700

    refactor: add MenuItem component

--- components/MenuCategory.vue
+++ components/MenuCategory.vue
@@ -11,25 +11,7 @@
         class="rounded h-24 w-24 mr-4"
       >
 
-      <div>
-        <h3 class="text-base px-1">
-          Seo Goo Lalapan Ikan Laut Bakar/Goreng (Banyak Pilihan Ikan)
-        </h3>
-
-        <h6 class="mt-1 text-xs text-secondary-light">
-          Ikan Laut+Nasi dibakar dengan bumbu Bikin Doyan
-        </h6>
-
-        <div class="mt-3" />
-
-        <div class="flex">
-          <font-awesome-icon
-            :icon="['fas', 'plus-square']"
-            size="2x"
-            class="ml-auto text-green"
-          />
-        </div>
-      </div>
+      <MenuItem />
     </div>
   </div>
 </template>
---
+++ components/MenuItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3 class="text-base px-1">
+      Seo Goo Lalapan Ikan Laut Bakar/Goreng (Banyak Pilihan Ikan)
+    </h3>
+
+    <h6 class="mt-1 text-xs text-secondary-light">
+      Ikan Laut+Nasi dibakar dengan bumbu Bikin Doyan
+    </h6>
+
+    <div class="mt-3" />
+
+    <div class="flex">
+      <font-awesome-icon
+        :icon="['fas', 'plus-square']"
+        size="2x"
+        class="ml-auto text-green"
+      />
+    </div>
+  </div>
+</template>
