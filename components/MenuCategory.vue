babe3facaeb43ce523559b747c816c588cd49864
yogaajie2 <yogaajie2@gmail.com>
2022-01-25 00:26:55 +0700

    refactor: add MenuItem component

--- components/MenuCategory.vue
+++ components/MenuCategory.vue
@@ -4,14 +4,10 @@
       Aigo
     </h2>
 
-    <div class="border-b border-secondary-lightest flex py-4">
-      <img
-        src="~/assets/img/img-1.jpeg"
-        alt="Do Yan Seafood - Sumbersekar : Seo Goo Lalapan Ikan Laut Bakar/Goreng (Banyak Pilihan Ikan)"
-        class="rounded h-24 w-24 mr-4"
-      >
-
-      <MenuItem />
-    </div>
+    <MenuItem
+      v-for="(item, index) in menu.menuItems"
+      :key="index"
+      :item="item"
+    />
   </div>
 </template>
--- components/MenuItem.vue
+++ components/MenuItem.vue
@@ -1,21 +1,29 @@
 <template>
-  <div>
-    <h3 class="text-base px-1">
-      Seo Goo Lalapan Ikan Laut Bakar/Goreng (Banyak Pilihan Ikan)
-    </h3>
+  <div class="border-b border-secondary-lightest flex py-4">
+    <img
+      src="~/assets/img/img-1.jpeg"
+      alt="Do Yan Seafood - Sumbersekar : Seo Goo Lalapan Ikan Laut Bakar/Goreng (Banyak Pilihan Ikan)"
+      class="rounded h-24 w-24 mr-4"
+    >
 
-    <h6 class="mt-1 text-xs text-secondary-light">
-      Ikan Laut+Nasi dibakar dengan bumbu Bikin Doyan
-    </h6>
+    <div>
+      <h3 class="text-base px-1">
+        Seo Goo Lalapan Ikan Laut Bakar/Goreng (Banyak Pilihan Ikan)
+      </h3>
 
-    <div class="mt-3" />
+      <h6 class="mt-1 text-xs text-secondary-light">
+        Ikan Laut+Nasi dibakar dengan bumbu Bikin Doyan
+      </h6>
 
-    <div class="flex">
-      <font-awesome-icon
-        :icon="['fas', 'plus-square']"
-        size="2x"
-        class="ml-auto text-green"
-      />
+      <div class="mt-3" />
+
+      <div class="flex">
+        <font-awesome-icon
+          :icon="['fas', 'plus-square']"
+          size="2x"
+          class="ml-auto text-green"
+        />
+      </div>
     </div>
   </div>
 </template>
