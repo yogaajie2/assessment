70c51f18de952c3b996aabdae5f642d6efc25a75
yogaajie2 <yogaajie2@gmail.com>
2022-01-24 15:18:39 +0700

    : init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
+  ],
+  content: [],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
