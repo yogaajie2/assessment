0350a0e488597421bf8374b1b85c10e2d70165c2
yogaajie2 <yogaajie2@gmail.com>
2022-01-25 12:11:56 +0700

    build(fontawesome): replace mapMarker with mapMarkerAlt

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faAngleDown, faAngleLeft, faAngleRight, faPlusSquare, faMapMarker } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faAngleLeft, faAngleRight, faPlusSquare, faMapMarkerAlt, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { faFacebookSquare, faInstagram, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
@@ -9,7 +9,7 @@ config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(faAngleDown, faAngleLeft, faAngleRight, faPlusSquare, faMapMarker, faFacebookSquare, faInstagram, faTwitterSquare)
+library.add(faAngleDown, faAngleLeft, faAngleRight, faPlusSquare, faMapMarkerAlt, faTimes, faFacebookSquare, faInstagram, faTwitterSquare)
 
 // Register the component globally
 // eslint-disable-next-line vue/component-definition-name-casing
