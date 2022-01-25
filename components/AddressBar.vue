673fc22877bd4918c120c7942ca82dfc8982bd36
yogaajie2 <yogaajie2@gmail.com>
2022-01-25 12:12:23 +0700

    fix(AddressBar): replace mapMarker with mapMarkerAlt

--- components/AddressBar.vue
+++ components/AddressBar.vue
@@ -1,12 +1,9 @@
 <template>
-  <section class="flex h-10 sticky items-center justify-center top-0 gap-2 z-10 bg-white">
-    <div class="flex justify-center items-center rounded-full p-1 w-5 bg-red">
-      <font-awesome-icon
-        :icon="['fas', 'map-marker']"
-        class="text-white"
-        size="xs"
-      />
-    </div>
+  <section class="flex h-10 sticky items-center justify-center top-0 gap-2 z-10 bg-white lg:border lg:border-secondary-lightest lg:px-6 lg:rounded lg:static">
+    <font-awesome-icon
+      :icon="['fas', 'map-marker-alt']"
+      class="text-red"
+    />
     <p class="text-sm text-secondary-lighter">
       Type your location
     </p>
