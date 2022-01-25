0aa1a2aa5feff295a6a87a93308969626e34d7d2
yogaajie2 <yogaajie2@gmail.com>
2022-01-25 10:12:52 +0700

    fix: lg breakpoint UI updates

--- components/FooterDark.vue
+++ components/FooterDark.vue
@@ -1,35 +1,66 @@
 <template>
-  <section class="pb-12 pt-8 px-3 text-center bg-primary-light text-white">
-    <p class="mb-4 font-medium text-base">
-      Countries with GrabFood
-    </p>
+  <section class="pb-12 pt-8 px-3 text-center bg-primary-light text-white lg:text-left">
+    <div class="container">
+      <div class="lg:flex lg:justify-between">
+        <div>
+          <p class="mb-4 font-medium text-base">
+            Countries with GrabFood
+          </p>
 
-    <div class="mb-8">
-      <a href="javascript:void(0)">Indonesia</a> | <a href="javascript:void(0)">Philippines</a> | <a href="javascript:void(0)">Thailand</a> | <a href="javascript:void(0)">Vietnam</a> | <a href="javascript:void(0)">Singapore</a> | <a href="javascript:void(0)">Malaysia</a> | <a href="javascript:void(0)">Myanmar</a>
-    </div>
+          <div class="mb-8">
+            <a href="javascript:void(0)">Indonesia</a> | <a href="javascript:void(0)">Philippines</a> | <a href="javascript:void(0)">Thailand</a> | <a href="javascript:void(0)">Vietnam</a> | <a href="javascript:void(0)">Singapore</a> | <a href="javascript:void(0)">Malaysia</a> | <a href="javascript:void(0)">Myanmar</a>
+          </div>
+        </div>
 
-    <div class="mb-6">
-      <p class="mb-2 text-base text-secondary-lighter">
-        &copy; Grab 2022
-      </p>
+        <div class="mb-3 space-x-4">
+          <a href="javascript:void(0)">
+            <font-awesome-icon
+              :icon="['fab', 'facebook-square']"
+              size="2x"
+            />
+          </a>
 
-      <div>
-        <a href="javascript:void(0)">Terms of Service</a>
-        <span class="mx-1">•</span>
-        <a href="javascript:void(0)">Privacy Policy</a>
+          <a href="javascript:void(0)">
+            <font-awesome-icon
+              :icon="['fab', 'instagram']"
+              size="2x"
+            />
+          </a>
+
+          <a href="javascript:void(0)">
+            <font-awesome-icon
+              :icon="['fab', 'twitter-square']"
+              size="2x"
+            />
+          </a>
+        </div>
       </div>
-    </div>
 
-    <div class="flex justify-center gap-6">
-      <img
-        src="~/assets/img/logo-appstore.svg"
-        class="h-9 w-28"
-      >
+      <div class="flex justify-between">
+        <div class="mb-6 lg:mb-0">
+          <p class="mb-2 text-base text-secondary-lighter">
+            &copy; Grab 2022
+          </p>
 
-      <img
-        src="~/assets/img/logo-playstore.svg"
-        class="h-9 w-28"
-      >
+          <div>
+            <a href="javascript:void(0)">Terms of Service</a>
+            <span class="mx-1">•</span>
+            <a href="javascript:void(0)">Privacy Policy</a>
+          </div>
+        </div>
+
+        <div class="flex justify-center gap-6">
+          <img
+            src="~/assets/img/logo-appstore.svg"
+            class="h-9 w-28"
+          >
+
+          <img
+            src="~/assets/img/logo-playstore.svg"
+            class="h-9 w-28"
+          >
+        </div>
+      </div>
     </div>
   </section>
 </template>
--- components/FooterNew.vue
+++ components/FooterNew.vue
@@ -1,55 +1,143 @@
 <template>
   <section class="pb-12 pt-8 px-3 bg-footernew text-primary-light">
-    <div class="border-b border-secondary-lighter flex items-center justify-between pb-6">
-      <a href="javascript:void(0)">
-        <img
-          src="~/assets/img/logo-grabfood-mono.svg"
-          class="w-23"
-        >
-      </a>
-
-      <div class="text-xs">
+    <div class="container">
+      <div class="border-b border-secondary-lighter flex items-center justify-between pb-6">
         <a href="javascript:void(0)">
-          EN
-
-          <font-awesome-icon
-            :icon="['fas', 'angle-down']"
-            class="ml-1"
-          />
+          <img
+            src="~/assets/img/logo-grabfood-mono.svg"
+            class="w-23"
+          >
         </a>
+
+        <div class="text-xs">
+          <a href="javascript:void(0)">
+            EN
+
+            <font-awesome-icon
+              :icon="['fas', 'angle-down']"
+              class="ml-1"
+            />
+          </a>
+        </div>
       </div>
-    </div>
 
-    <div class="flex items-center justify-between my-4">
-      <p class="font-medium text-base">
-        Frequently Searched
-      </p>
+      <div class="lg:flex lg:justify-between">
+        <div class="flex items-center justify-between my-4 lg:flex-col lg:items-baseline lg:justify-start">
+          <p class="font-medium text-base">
+            Frequently Searched
+          </p>
 
-      <font-awesome-icon :icon="['fas', 'angle-down']" />
-    </div>
+          <ul>
+            <li>
+              <a href="javascript:void(0)">Burger King Menu</a>
+            </li>
 
-    <div class="flex items-center justify-between my-4">
-      <p class="font-medium text-base">
-        Popular Cuisines
-      </p>
+            <li>
+              <a href="javascript:void(0)">Chatime Menu</a>
+            </li>
 
-      <font-awesome-icon :icon="['fas', 'angle-down']" />
-    </div>
+            <li>
+              <a href="javascript:void(0)">Domino's Pizza Menu</a>
+            </li>
 
-    <div class="flex items-center justify-between my-4">
-      <p class="font-medium text-base">
-        About Grab
-      </p>
+            <li>
+              <a href="javascript:void(0)">Hokben Menu</a>
+            </li>
 
-      <font-awesome-icon :icon="['fas', 'angle-down']" />
-    </div>
+            <li>
+              <a href="javascript:void(0)">JCO Delivery</a>
+            </li>
+          </ul>
 
-    <div class="flex items-center justify-between my-4">
-      <p class="font-medium text-base">
-        Support
-      </p>
+          <div class="lg:hidden">
+            <font-awesome-icon :icon="['fas', 'angle-down']" />
+          </div>
+        </div>
 
-      <font-awesome-icon :icon="['fas', 'angle-down']" />
+        <div class="flex items-center justify-between my-4 lg:flex-col lg:items-baseline lg:justify-start">
+          <p class="font-medium text-base">
+            Popular Cuisines
+          </p>
+
+          <ul>
+            <li>
+              <a href="javascript:void(0)">Chinese Food</a>
+            </li>
+
+            <li>
+              <a href="javascript:void(0)">Fast Food</a>
+            </li>
+
+            <li>
+              <a href="javascript:void(0)">Indian Food</a>
+            </li>
+
+            <li>
+              <a href="javascript:void(0)">Indonesian Food</a>
+            </li>
+
+            <li>
+              <a href="javascript:void(0)">Italian Food</a>
+            </li>
+          </ul>
+
+          <div class="lg:hidden">
+            <font-awesome-icon :icon="['fas', 'angle-down']" />
+          </div>
+        </div>
+
+        <div class="flex items-center justify-between my-4 lg:flex-col lg:items-baseline lg:justify-start">
+          <p class="font-medium text-base">
+            About Grab
+          </p>
+
+          <ul>
+            <li>
+              <a href="javascript:void(0)">About Grab</a>
+            </li>
+
+            <li>
+              <a href="javascript:void(0)">About GrabFood</a>
+            </li>
+
+            <li>
+              <a href="javascript:void(0)">Blog</a>
+            </li>
+          </ul>
+
+          <div class="lg:hidden">
+            <font-awesome-icon :icon="['fas', 'angle-down']" />
+          </div>
+        </div>
+
+        <div class="flex items-center justify-between my-4 lg:flex-col lg:items-baseline lg:justify-start">
+          <p class="font-medium text-base">
+            Support
+          </p>
+
+          <ul>
+            <li>
+              <a href="javascript:void(0)">Help</a>
+            </li>
+
+            <li>
+              <a href="javascript:void(0)">FAQs</a>
+            </li>
+
+            <li>
+              <a href="javascript:void(0)">Be a GrabFood Merchant</a>
+            </li>
+
+            <li>
+              <a href="javascript:void(0)">Drive With Grab</a>
+            </li>
+          </ul>
+
+          <div class="lg:hidden">
+            <font-awesome-icon :icon="['fas', 'angle-down']" />
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
