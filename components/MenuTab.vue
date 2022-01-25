3ab65ec41fe993e619126e156f9a045624ca3698
yogaajie2 <yogaajie2@gmail.com>
2022-01-25 12:17:58 +0700

    fix(MenuTab): implement some UI fixes

--- components/MenuTab.vue
+++ components/MenuTab.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="sticky top-10 z-10 bg-white">
+  <div class="sticky top-10 z-10 bg-white lg:top-23">
     <div class="container flex items-center justify-between">
       <font-awesome-icon
         :icon="['fas', 'angle-left']"
-        class="text-secondary-light"
+        class="text-secondary-light cursor-pointer"
       />
 
       <div class="flex justify-between">
@@ -11,12 +11,13 @@
           v-for="(category, index) in categories"
           :key="index"
           :href="`#${category.categoryName}`"
+          class="p-3 text-sm text-secondary transition-colors duration-200 lg:px-6 lg:py-3 lg:text-base lg:hover:text-green"
         >{{ category.categoryName }}</a>
       </div>
 
       <font-awesome-icon
         :icon="['fas', 'angle-right']"
-        class="text-secondary-light"
+        class="text-secondary-light cursor-pointer"
       />
     </div>
   </div>
