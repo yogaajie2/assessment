e661f990ff6fced3e23920b4cade4a9c17d5b7ca
yogaajie2 <yogaajie2@gmail.com>
2022-01-25 12:59:54 +0700

    feat: toggle dropdowns

--- components/DateDropdownSelect.vue
+++ components/DateDropdownSelect.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="border border-secondary-lightest h-12 px-2 rounded-md flex items-center text-sm transition duration-200 cursor-pointer lg:w-1/3 hover:border-green">
+  <div
+    class="border border-secondary-lightest h-12 px-2 rounded-md flex items-center relative text-sm transition duration-200 cursor-pointer lg:w-1/3 hover:border-green"
+    @click="isDateDropdownShown = !isDateDropdownShown"
+  >
     <img
       src="~/assets/img/icon-calendar.svg"
       class="pr-2"
@@ -10,14 +13,29 @@
       class="ml-auto text-secondary-lighter"
       size="sm"
     />
+
+    <ul
+      v-show="isDateDropdownShown"
+      class="absolute -bottom-36 left-0 p-4 rounded shadow-lg w-full z-20 bg-white"
+    >
+      <li class="py-2">
+        Today
+      </li>
+      <li class="py-2">
+        Tue, 25 Jan
+      </li>
+      <li class="py-2">
+        Wed, 26 Jan
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    handleChange(value) {
-      // console.log(`selected ${value}`)
+  data() {
+    return {
+      isDateDropdownShown: false
     }
   }
 }
--- components/TimeDropdownSelect.vue
+++ components/TimeDropdownSelect.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="border border-secondary-lightest h-12 px-2 rounded-md flex items-center text-sm transition duration-200 cursor-pointer lg:w-1/3 hover:border-green">
+  <div
+    class="border border-secondary-lightest h-12 px-2 rounded-md flex items-center relative text-sm transition duration-200 cursor-pointer lg:w-1/3 hover:border-green"
+    @click="isTimeDropdownShown = !isTimeDropdownShown"
+  >
     <img
       src="~/assets/img/icon-clock.svg"
       class="pr-2"
@@ -10,14 +13,28 @@
       class="ml-auto text-secondary-lighter"
       size="sm"
     />
+    <ul
+      v-show="isTimeDropdownShown"
+      class="absolute -bottom-36 left-0 p-4 rounded shadow-lg w-full z-20 bg-white"
+    >
+      <li class="py-2">
+        14:30 - 14:45
+      </li>
+      <li class="py-2">
+        14:45 - 15:00
+      </li>
+      <li class="py-2">
+        15:00 - 15:15
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    handleChange(value) {
-      // console.log(`selected ${value}`)
+  data() {
+    return {
+      isTimeDropdownShown: false
     }
   }
 }
