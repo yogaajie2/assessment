915642c4060f9a53155314ccc365f2a306f13542
yogaajie2 <yogaajie2@gmail.com>
2022-01-25 10:12:38 +0700

    build(fontawesome): add brands icons

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faAngleDown, faPlusSquare, faMapMarker } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookSquare, faInstagram, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
 config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(faAngleDown, faPlusSquare, faMapMarker)
+library.add(faAngleDown, faPlusSquare, faMapMarker, faFacebookSquare, faInstagram, faTwitterSquare)
 
 // Register the component globally
 // eslint-disable-next-line vue/component-definition-name-casing
